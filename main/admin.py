cfee153fa5c0fc5a6ca71ad917e9cc6783257c29
hindkablawat <hindkablawat@gmail.com>
2026-01-26 22:50:43 +0100

    Register JobApplication model in admin

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import StudentProfile, Job
+from .models import StudentProfile, Job, JobApplication
 
 admin.site.register(StudentProfile)
 admin.site.register(Job)
+admin.site.register(JobApplication)
